e9865f3d360d9548d1b195efd05630d27090097b
rh0danthe <rita.pichushkina.04@mail.ru>
2023-12-10 14:25:50 +0300

    added dto

---
+++ src/main/java/com/webJava/library/dto/auth/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.webJava.library.dto.auth;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+@Data
+public class LoginRequest {
+    @Size(min = 3, max = 30)
+    private String username;
+
+    @Pattern(regexp = "(?=\\w*[a-z])(?=\\w*[A-Z])(?=\\w*[0-9])\\w{8,}")
+    private String password;
+}
